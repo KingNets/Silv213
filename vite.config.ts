0877e10a68c579202dd6130638ebff917035c0cb
KingNets <kingnets@example.com>
2025-12-29 11:13:26 +0200

    Setup GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Silv213/',
   plugins: [react()],
   resolve: {
     alias: {
@@ -12,6 +13,5 @@ export default defineConfig({
   },
   server: {
     port: 3000,
-
   },
 })
